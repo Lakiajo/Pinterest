6147274afb32b2766ef44c0d05591d6303c75544
Lakia Jones <rachandra_jones@yahoo.com>
2019-05-14 20:58:17 -0500

    working

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -1,9 +1,27 @@
 import pinsData from '../../helpers/data/pinsData';
+import util from '../../helpers/util';
+
+const bindEvents = () => {
+  document.getElementById('toBoardsBtn').addEventListener('click', () => {
+    document.getElementById('boards-page').classList.remove('hide');
+    document.getElementById('pins-page').classList.add('hide');
+  });
+};
+
+const printPins = (pins) => {
+  let domstring = '';
+  pins.forEach((pin) => {
+    domstring += `<img src="${pin.imageUrl}" alt="pin Image">`;
+  });
+  util.printToDom('pins-on-board', domstring);
+};
 
 const initPins = (boardId) => {
+  bindEvents();
   pinsData.loadPinsForBoard(boardId)
     .then((pins) => {
       console.error('All pins', pins);
+      printPins(pins);
     })
     .catch(err => console.error(err));
 };
